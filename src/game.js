10e332e773267a02a8d74cca05118a5ef9221981
ruohanhuang971 <ruohanhuang971@gmail.com>
2024-02-07 22:13:38 -0800

    add light

--- src/game.js
+++ src/game.js
@@ -1,15 +1,23 @@
 import * as THREE from "three";
-export default class FPSScene extends THREE.Scene {
-    initialize() {
+export default class Game {
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+        );
+
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshPhongMaterial({color: 0x00ff00});
-        
         this.cube = new THREE.Mesh(geometry, material);
-        this.add(this.cube);
+        this.scene.add(this.cube);
+        this.camera.position.z = 5;
 
         const light = new THREE.DirectionalLight(0xFFFFFF, 1);
         light.position.set(0, 4, 2);
-        this.add(light);
+        this.scene.add(light);
     }
 
     update() {
--- src/main.js
+++ src/main.js
@@ -1,30 +1,17 @@
 import * as THREE from "three";
-import FPSScene from "./FPSScene.js";
-
-const width = window.innerWidth;
-const height = window.innerHeight
+import Game from "./game.js"
 
 window.onload = () => {
     const renderer = new THREE.WebGLRenderer({
         canvas: document.getElementById('app')
     });
-    renderer.setSize(width, height);
-    
-    const mainCamera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-        );
-    mainCamera.position.z = 5;
-    // this.scene = new THREE.Scene();
-    const scene = new FPSScene();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-
+    const game = new Game();
     function animate() {
         requestAnimationFrame(animate);
-        scene.update();
-        renderer.render(game.scene, game.mainCamera);
+        game.update();
+        renderer.render(game.scene, game.camera);
     }
     animate();
 }
