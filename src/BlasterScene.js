ab92ef13abd79d027dd667de39fe23dbfce4942d
Benjamin Huang <benjhuang@gmail.com>
2024-02-17 14:48:20 -0800

    fix BlasterScene.js

--- src/BlasterScene.js
+++ src/BlasterScene.js
@@ -1,24 +1,32 @@
 import * as THREE from "three";
 
-import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
-import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader';
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
+import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
 
 export default class BlasterScene extends THREE.Scene {
-    static mtlLoader = new MTLLoader();
-    static objLoader = new OBJLoader(); 
+  mtlLoader = new MTLLoader();
+  objLoader = new OBJLoader();
 
-    initialize() {
-        const targetMtl = this.mtlLoader.loadAsync('../public/assets/targetB.mtl');
-        targetMtl.preload();
+  async initialize() {
+    const targetMtl = await this.mtlLoader.loadAsync(
+      "../public/assets/targetB.mtl"
+    );
+    targetMtl.preload();
 
-        const light = new THREE.DirectionalLight(0xFFFFFF, 1);
-        light.position.set(0, 4, 2);
-        
-        this.add(light);
-    }
+    this.objLoader.setMaterials(targetMtl);
+    const model = await this.objLoader.loadAsync(
+      "../public/assets/targetB.obj"
+    );
+    this.add(model);
 
-    update() {
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
-    }
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 4, 2);
+
+    this.add(light);
+  }
+
+  update() {
+    // this.cube.rotation.x += 0.01;
+    // this.cube.rotation.y += 0.01;
+  }
 }
